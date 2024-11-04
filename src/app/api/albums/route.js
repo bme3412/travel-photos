0f23f97a59a2c5a250a773d07f484939bfeaaad6
Brendan Erhard <erhardbr@gmail.com>
2024-11-03 20:36:53 -0500

    fixed img

--- src/app/api/albums/route.js
+++ src/app/api/albums/route.js
@@ -2,6 +2,6 @@
 import { NextResponse } from 'next/server';
 import albums from '../../../data/albums.json';
 
-export async function GET(request) {
+export async function GET() {  // Removed unused request parameter
   return NextResponse.json(albums);
 }
